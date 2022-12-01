0e4a710f17dd0218a53de52737f6660ff9ca12e6
Tim Mecklem <timothy@mecklem.com>
2022-12-01 17:07:44 -0500

    Add GitHub Actions for build and hex publish

--- mix.exs
+++ mix.exs
@@ -10,7 +10,7 @@ defmodule BumperCrop.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       package: package(),
       start_permanent: Mix.env() == :prod,
-      version: "0.1.0",
+      version: "0.1.0"
     ]
   end
 
@@ -28,8 +28,9 @@ defmodule BumperCrop.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 
