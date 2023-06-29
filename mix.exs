5f3b51dde74cc184a0050b4e665b83ec79285d23
Tim Mecklem <timothy@mecklem.com>
2023-06-29 14:52:13 -0400

    Version bump

--- mix.exs
+++ mix.exs
@@ -10,7 +10,7 @@ defmodule BumperCrop.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       package: package(),
       start_permanent: Mix.env() == :prod,
-      version: "0.1.4"
+      version: "0.2.0"
     ]
   end
 
